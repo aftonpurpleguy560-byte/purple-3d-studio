27cd12d42669223584d1fdba3c9b2cb0326eb3ea
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-03-20 03:26:01 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'purple-studio-v1';
+const assets = ['/', '/index.html', '/style.css', '/main.js', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
